3997b6fe4083b2dece5975492c3fd45ff991fefb
lpshkn <kofffsh@gmail.com>
2020-12-02 19:28:44 +0300

    Add tests for the utils.py module

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -1,6 +1,8 @@
 import os
 import unittest.mock
-import ataurus.configurator as cfg
+import ataurus.configurator.configurator as cfg
+from ataurus.configurator.utils import (MODEL_FILE, CACHE_DIRECTORY, CACHE_CFG_FILE, POSTFIX_CACHE_FEATURES,
+                                        convert_to_cache_name, convert_from_cache_name, get_hash)
 
 
 class ConfiguratorTest(unittest.TestCase):
@@ -51,3 +53,29 @@ class ConfiguratorTest(unittest.TestCase):
         args = ['-f', self.correct_file]
         configurator = cfg.Configurator(args)
         data = configurator.data
+
+
+class ConfiguratorUtilsTest(unittest.TestCase):
+    def test_converting_to_cache(self):
+        filename = 'file.csv'
+        self.assertEqual(convert_to_cache_name(filename), 'file' + POSTFIX_CACHE_FEATURES + '.csv')
+        self.assertEqual(convert_to_cache_name(filename, 'pickle'), 'file' + POSTFIX_CACHE_FEATURES + '.pickle')
+        filename = 'file.exe.csv'
+        self.assertEqual(convert_to_cache_name(filename, 'pickle'), 'file.exe' + POSTFIX_CACHE_FEATURES + '.pickle')
+        self.assertEqual(convert_to_cache_name(filename, 'pickle'), 'file.exe' + POSTFIX_CACHE_FEATURES + '.pickle')
+        filename = 'file'
+        self.assertEqual(convert_to_cache_name(filename), 'file' + POSTFIX_CACHE_FEATURES)
+        self.assertEqual(convert_to_cache_name(filename, 'pickle'), 'file' + POSTFIX_CACHE_FEATURES + '.pickle')
+
+    def test_converting_from_cache(self):
+        filename = 'file_features.csv'
+        self.assertEqual(convert_from_cache_name(filename), 'file' + '.csv')
+        self.assertEqual(convert_from_cache_name(filename, 'pickle'), 'file' + '.pickle')
+        filename = 'file.exe_features.csv'
+        self.assertEqual(convert_from_cache_name(filename, 'pickle'), 'file.exe' + '.pickle')
+        filename = 'file_features'
+        self.assertEqual(convert_from_cache_name(filename, 'pickle'), 'file.pickle')
+        self.assertEqual(convert_from_cache_name(filename), 'file')
+        filename = 'file'
+        with self.assertRaises(ValueError):
+            convert_from_cache_name(filename, 'pickle')
