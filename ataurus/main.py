1226d2e05bab3747021001ed40733439ad41c771
lpshkn <kofffsh@gmail.com>
2020-12-02 16:30:21 +0300

    Add a block in the main function that returns either an object of FeaturesExtractor or DataFrame's object from the input property of Configurator

--- ataurus/main.py
+++ ataurus/main.py
@@ -1,5 +1,6 @@
 import sys
-import configurator as cfg
+import configurator.configurator as cfg
+import pandas as pd
 from preparing.preparator import Preparator
 from features.extractor import FeaturesExtractor
 from ml.model import Model
@@ -7,24 +8,26 @@ from ml.model import Model
 
 def main():
     configurator = cfg.Configurator(sys.argv[1:])
+    input_obj = configurator.input
+    if configurator.command == 'train' or configurator.command == 'predict':
+        if isinstance(input_obj, FeaturesExtractor):
+            extractor = input_obj
+        elif isinstance(input_obj, pd.DataFrame):
+            preparator = Preparator().fit(input_obj)
+            extractor = FeaturesExtractor().fit(preparator.tokens(), preparator.sentences(), preparator.authors)
+            configurator.to_cache(extractor)
 
     if configurator.command == 'info':
         model = configurator.model
         model.info()
 
     elif configurator.command == 'train':
-        preparator = Preparator().fit(configurator.input_data)
-        extractor = FeaturesExtractor().fit(preparator.tokens(), preparator.sentences(), preparator.authors)
         print('FEATURES:\n', extractor.features)
-
         model = Model()
         model.fit(extractor.X, extractor.y)
         model.save(configurator.output_file)
 
     elif configurator.command == 'predict':
-        preparator = Preparator().fit(configurator.input_data)
-        extractor = FeaturesExtractor().fit(preparator.tokens(), preparator.sentences(), preparator.authors)
-
         model = configurator.model
         print('Predictions:', model.predict(extractor.X))
 
