77fed4157ab93c9750685fc45f0160ec7ed2e5db
lpshkn <lepkirill@yandex.ru>
2021-03-22 23:39:10 +0300

    Update tests for the Database module due to using Elasticsearch DSL

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,15 +1,16 @@
 from ataurus.database.db import Database
+from ataurus.database.model import Article
 import unittest
 import elasticsearch
+import elasticsearch_dsl as es_dsl
 
 
 class DatabaseTest(unittest.TestCase):
     def setUp(self) -> None:
         self.connection = Database.connect(['localhost:9200']).connection
-        self.test_index = 'test'
 
     def tearDown(self) -> None:
-        self.connection.indices.delete(index=self.test_index, ignore=[400, 404])
+        self.connection.indices.delete(index=Article.Index.name, ignore=[400, 404])
 
     def test_correct_connection(self):
         self.assertTrue(self.connection.ping())
@@ -19,14 +20,8 @@ class DatabaseTest(unittest.TestCase):
             Database.connect(["wrong_address:9500"])
 
     def test_creating_index(self):
-        self.connection.indices.create(self.test_index)
-        self.assertTrue(self.connection.indices.exists(self.test_index))
-
-    def test_incorrect_index(self):
-        self.connection.indices.create(self.test_index)
-        with self.assertRaises(elasticsearch.exceptions.RequestError) as e:
-            self.connection.indices.create(self.test_index)
-            self.assertEqual(e.exception.status_code, 400)
+        Article.init(using=self.connection)
+        self.assertTrue(self.connection.indices.exists(Article.Index.name))
 
 
 if __name__ == '__main__':
