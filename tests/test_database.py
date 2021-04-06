19d17475cd10c7692c632227675889ebca2e2442
Lpshkn <lepkirill@yandex.ru>
2021-04-07 00:35:27 +0300

    Add test for the getting texts method

--- tests/test_database.py
+++ tests/test_database.py
@@ -12,6 +12,11 @@ class DatabaseTest(unittest.TestCase):
         self.correct_host = f'{ES_HOST}:{ES_PORT}'
         self.incorrect_host = 'wrong'
         self.db = Database.connect([self.correct_host])
+        self.test_index = 'test_index'
+        self.db.connection.index(index=self.test_index, body={'author': 'test', 'text': 'test'})
+
+    def tearDown(self) -> None:
+        self.db.connection.indices.delete(self.test_index)
 
     def test_correct_connection(self):
         self.assertTrue(self.db.connection.ping())
@@ -19,6 +24,11 @@ class DatabaseTest(unittest.TestCase):
     def test_wrong_connection(self):
         with self.assertRaises(ConnectionError):
             Database.connect([self.incorrect_host])
+
+    def test_get_texts(self):
+        authors, texts = self.db.get_texts(self.test_index, 'author', 'text')
+        self.assertEqual(authors, ['test'])
+        self.assertEqual(texts, ['test'])
 
 
 if __name__ == '__main__':
