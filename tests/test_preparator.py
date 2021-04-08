cd18428b8ff141763d16ef1e278aad6b4ebf2448
Lpshkn <lepkirill@yandex.ru>
2021-04-08 22:56:16 +0300

    Update a test for the preprocessing method

--- tests/test_preparator.py
+++ tests/test_preparator.py
@@ -5,25 +5,33 @@ from ataurus.preparing.preprocessor import Preprocessor
 
 class PreprocessorTest(unittest.TestCase):
     def setUp(self):
-        self.test_texts = ['ЭТО первый ТЕКСТ', 'Это Второй Тестовый текст', '', 'Прежде был пустой текст, теперь нет']
+        self.test_texts = ['ЭТО первый ТЕКСТ', 'Это Второй Тестовый текст', '', 'Прежде был пустой   текст, теперь нет',
+                           "РАЗРАБОТЧИКИ?, !$ LinkedIn    \n\n  объявили о появившейся возможности#%^^%!@#$%^&*(? "
+                           "https://google.com/"]
 
     def tearDown(self):
         pass
 
     def test_preprocess_text(self):
-        texts = ["РАЗРАБОТЧИКИ LinkedIn    \n\n  объявили о появившейся возможности", "https://google.com/", "author"]
-
-        self.assertEqual(Preprocessor.preprocess_text(texts[0]),
-                         'разработчики linkedin объявили о появившейся возможности')
-        self.assertEqual(Preprocessor.preprocess_text(texts[1]), '')
-        self.assertEqual(Preprocessor.preprocess_text(texts[2]), 'author')
-        self.assertEqual(Preprocessor.preprocess_text(self.test_texts[2]), '')
-        self.assertEqual(Preprocessor.preprocess_text("РАЗРАБОТЧИКИ LinkedIn", lower=False), 'РАЗРАБОТЧИКИ LinkedIn')
-        self.assertEqual(Preprocessor.preprocess_text(texts[0], lower=False, delete_whitespace=False), texts[0])
-        self.assertEqual(Preprocessor.preprocess_text(texts[0] + ' ' + texts[1], lower=False, delete_whitespace=False),
-                         texts[0])
-        self.assertEqual(Preprocessor.preprocess_text(texts[0] + ' ' + texts[1], lower=False, delete_whitespace=False,
-                                                      delete_urls=False), texts[0] + ' ' + texts[1])
+        self.assertEqual(Preprocessor.preprocess_text(self.test_texts[4],
+                                                      lower=True, delete_whitespace=True, delete_urls=True),
+                         'разработчики?, ! linkedin объявили о появившейся возможности!?')
+        self.assertEqual(Preprocessor.preprocess_text(self.test_texts[0],
+                                                      lower=True, delete_whitespace=True, delete_urls=True),
+                         'это первый текст')
+        self.assertEqual(Preprocessor.preprocess_text(self.test_texts[1],
+                                                      lower=False, delete_whitespace=True, delete_urls=True),
+                         'Это Второй Тестовый текст')
+        self.assertEqual(Preprocessor.preprocess_text(self.test_texts[3],
+                                                      lower=False, delete_whitespace=False, delete_urls=True),
+                         'Прежде был пустой   текст, теперь нет')
+        self.assertEqual(Preprocessor.preprocess_text(self.test_texts[4],
+                                                      lower=False, delete_whitespace=False, delete_urls=True),
+                         "РАЗРАБОТЧИКИ?, ! LinkedIn    \n\n  объявили о появившейся возможности!?")
+        self.assertEqual(Preprocessor.preprocess_text(self.test_texts[4],
+                                                      lower=False, delete_whitespace=False, delete_urls=False),
+                         "РАЗРАБОТЧИКИ?, ! LinkedIn    \n\n  объявили о появившейся возможности!?"
+                         " httpsgoogle.com")
 
     def test_tokens_method(self):
         df = pd.DataFrame([["РАЗРАБОТЧИКИ?, !$ LinkedIn    \n\n  объявили о появившейся возможности#%^^%!@#$%^&*(?"
