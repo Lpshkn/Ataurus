5e6bcce454083b5d83e2866f53ccf84f19fae9d7
Lpshkn <lepkirill@yandex.ru>
2021-04-09 23:59:36 +0300

    Create own sklearn-based estimator-transformer to extract the features matrix

--- ataurus/features/extractor.py
+++ ataurus/features/extractor.py
@@ -6,7 +6,45 @@ import pandas as pd
 import ataurus.features.functions as funcs
 import tqdm
 import time
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from ataurus.preparing.preprocessor import Preprocessor
+
+
+class FeaturesExtractor(BaseEstimator, TransformerMixin):
+    def __init__(self, avg_words=True, avg_sentences=True, pos_distribution=True,
+                 foreign_words_ratio=True, vocabulary_richness=True, punctuation_distribution=True):
+        """
+        Extractor of features matrix. All parameters are flags that specify to include a result of processing
+        of each method to the final result.
+
+        :param avg_words: an average length of all words
+        :param avg_sentences: an average length of all sentences
+        :param pos_distribution: a part of speech distribution
+        :param foreign_words_ratio: ratio of foreign words count / count of all words
+        :param vocabulary_richness: a lexicon size
+        :param punctuation_distribution: a distribution of punctuation symbols
+        """
+        self.avg_words = avg_words
+        self.avg_sentences = avg_sentences
+        self.pos_distribution = pos_distribution
+        self.foreign_words_ratio = foreign_words_ratio
+        self.vocabulary_richness = vocabulary_richness
+        self.punctuation_distribution = punctuation_distribution
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X: dict, y=None):
+        tokens = X['tokens']
+        sentences = X['sentences']
+        texts = X['texts']
+
+        if tokens is None or sentences is None:
+            preprocessor = Preprocessor(texts)
+
+        if self.avg_words:
+            funcs.avg_len_words(tokens)
 
 
 class FeaturesExtractor:
