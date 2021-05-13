71466a20edf99f60684510c6ddbdec0a2215331a
Lpshkn <lepkirill@yandex.ru>
2021-05-13 21:52:04 +0300

    Replace the name of models to instances of Sklearn classes of Models
    It will allow to search hyper parameters of models in GridSearchCV directly.

--- ataurus/ml/model.py
+++ ataurus/ml/model.py
@@ -6,26 +6,20 @@ from sklearn.metrics import f1_score
 
 
 class Model(BaseEstimator, ClassifierMixin):
-    def __init__(self, estimator='RandomForest'):
+    def __init__(self, estimator=RandomForestClassifier()):
         """
-        :param estimator: estimator can be 'RandomForest', 'SVM'.
+        :param estimator: estimator can be 'RandomForest', 'SVC'.
         """
         self.estimator = estimator
 
     def fit(self, X, y):
         """
         Fit the model.
+
         :param X: {array-like, sparse matrix, dataframe} of shape (n_samples, n_features)
         :param y: ndarray of shape (n_samples,) Target values.
         :return: self
         """
-        if self.estimator == 'RandomForest':
-            self.estimator = RandomForestClassifier()
-        elif self.estimator == 'SVM':
-            self.estimator = SVC()
-        else:
-            raise ValueError('You chosen an incorrect estimator')
-
         # Return digit-view of y
         self.classes_, y = np.unique(y, return_inverse=True)
 
