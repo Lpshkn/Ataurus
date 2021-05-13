ada3c1883cddefb9f94034eccba7004e97adc41f
Lpshkn <lepkirill@yandex.ru>
2021-05-13 21:57:29 +0300

    Add searching hyper parameters of SVC and RandomForest models

--- ataurus/main.py
+++ ataurus/main.py
@@ -6,12 +6,13 @@ from ataurus.features.extractor import FeaturesExtractor
 from ataurus.database.client import Database
 from ataurus.ml.model import Model
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 
 
 def main():
-    # configurator = cfg.Configurator(sys.argv[1:])
     database = Database.connect(['localhost:9200'])
     authors, texts = database.get_authors_texts('articles_5ath', 'author_nickname', 'text')
     preprocessor = Preprocessor(texts)
@@ -29,30 +30,28 @@ def main():
     ])
 
     param_grid = [
-        {'extracting__avg_words': [True],
-         'extracting__avg_sentences': [True, False],
-         'extracting__pos_distribution': [True, False],
-#         'extracting__foreign_words_ratio': [True, False],
-         'extracting__vocabulary_richness': [True, False],
-#         'extracting__punctuation_distribution': [True, False],
-         'model__remove_nan': [True, False],
-         'model__estimator': ['RandomForest', 'SVM']}
+        {
+            # Hyper-parameters of the RandomForest
+            'model__estimator': [RandomForestClassifier()],
+            'model__estimator__n_estimators': [50, 100, 150, 200],
+            'model__estimator__criterion': ['gini', 'entropy'],
+            'model__estimator__max_depth': [2, 4, 6, 8, 10],
+            'model__estimator__min_samples_split': [2, 3, 4, 5],
+            'model__estimator__min_samples_leaf': [1, 2, 5, 10]},
+        {
+            # Hyper-parameters of the SVM
+            'model__estimator': [SVC()],
+            'model__estimator__C': [1, 5, 10, 20, 30],
+            'model__estimator__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+            'model__estimator__degree': [2, 3, 4]
+        }
     ]
-    texts = np.array(preprocessor.texts(), dtype=object)
-    tokens = np.array(preprocessor.tokens(), dtype=object)
-    sentences = np.array(preprocessor.sentences(), dtype=object)
-    X = np.c_[texts, tokens, sentences]
 
-    grid_search = GridSearchCV(pipeline, param_grid, n_jobs=-1, cv=2)
-
-    #grid_search.fit(np.c_[texts, np.full(len(texts), None), np.full(len(texts), None)], np.array(authors).ravel())
-    grid_search.fit(X, np.array(authors).ravel())
-    grid_search
-
-#    if configurator.command == 'train':
-#        pass
-#    elif configurator.command == 'predict':
-#        pass
+    random_search = RandomizedSearchCV(pipeline, param_grid, n_iter=20, n_jobs=-1, cv=3)
+    random_search.fit(X, y)
+    print(random_search.best_estimator_)
+    print(random_search.best_params_)
+    print(random_search.best_score_)
 
 
 if __name__ == '__main__':
