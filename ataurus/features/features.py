9d9857cb6ec1b485396824a3926a99d9a65d1c3a
Lpshkn <lepkirill@yandex.ru>
2021-05-17 00:39:21 +0300

    Add description for each feature

---
+++ ataurus/features/features.py
@@ -0,0 +1,9 @@
+# Description of each feature
+FEATURES = {
+    'avg_words': 'Average length of words',
+    'avg_sentences': 'Average length of sentences',
+    'pos_distribution': 'Part of speech distribution',
+    'foreign_words_ratio': 'Ratio of foreign words',
+    'lexicon': 'Lexicon size',
+    'punctuation_distribution': 'Punctuations distribution'
+}
