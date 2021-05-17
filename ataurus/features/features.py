eb32e7568c4b45979ff4929930927aa139820d7d
Lpshkn <lepkirill@yandex.ru>
2021-05-18 01:11:41 +0300

    Define the names of features using constant values

--- ataurus/features/features.py
+++ ataurus/features/features.py
@@ -1,9 +1,17 @@
+# Names of features
+AVG_WORDS = 'avg_words'
+AVG_SENTENCES = 'avg_sentences'
+POS_DISTRIBUTION = 'pos_distribution'
+FOREIGN_RATIO = 'foreign_words_ratio'
+LEXICON_SIZE = 'lexicon'
+PUNCTUATIONS_DISTRIBUTION = 'punctuation_distribution'
+
 # Description of each feature
-FEATURES = {
-    'avg_words': 'Average length of words',
-    'avg_sentences': 'Average length of sentences',
-    'pos_distribution': 'Part of speech distribution',
-    'foreign_words_ratio': 'Ratio of foreign words',
-    'lexicon': 'Lexicon size',
-    'punctuation_distribution': 'Punctuations distribution'
+FEATURES_DESCRIPTION = {
+    AVG_WORDS: 'Average length of words',
+    AVG_SENTENCES: 'Average length of sentences',
+    POS_DISTRIBUTION: 'Part of speech distribution',
+    FOREIGN_RATIO: 'Ratio of foreign words',
+    LEXICON_SIZE: 'Lexicon size',
+    PUNCTUATIONS_DISTRIBUTION: 'Punctuations distribution'
 }
