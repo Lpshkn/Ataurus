6cc0dec5a53e3c8e8c9aa7bb4762e413b0e6e187
Lpshkn <lepkirill@yandex.ru>
2021-05-22 00:47:06 +0300

    Update the main function

--- ataurus/main.py
+++ ataurus/main.py
@@ -1,7 +1,5 @@
 import sys
-
 import pandas as pd
-
 import console_handle.console_handler as cfg
 import numpy as np
 from preparing.preprocessor import Preprocessor
@@ -20,62 +18,68 @@ from serialize.features import serialize_features
 def main():
     console_handler = cfg.ConsoleHandler(sys.argv[1:])
 
+    input_data = console_handler.input
+    if type(input_data) == tuple:
+        if type(input_data[0]) == pd.DataFrame:
+            X, y = input_data
+        else:
+            texts, authors = input_data
+
+            # Extract lists of texts, tokens and sentences
+            preprocessor = Preprocessor(texts, authors)
+            texts = preprocessor.texts()
+            tokens = preprocessor.tokens()
+            sentences = preprocessor.sentences()
+            authors = preprocessor.authors
+            X = np.c_[texts, tokens, sentences]
+            y = np.array(authors).ravel()
+
+            # Extract features from texts, tokens and sentences
+            extractor = FeaturesExtractor(n_jobs=-1)
+            X = extractor.fit_transform(X)
+            # Serialize extracted features if it's necessary
+            if console_handler.features_path:
+                serialize_features(X, console_handler.features_path, authors=y)
+
+        # Remove null rows from texts and authors lists
+        notnull_indexes = X.notnull().all(axis=1)
+        X = X[notnull_indexes]
+        y = y[notnull_indexes]
+    else:
+        X = input_data
+        # Remove null rows from texts and authors lists
+        notnull_indexes = X.notnull().all(axis=1)
+        X = X[notnull_indexes]
+
     if console_handler.mode == 'train':
-        input_data = console_handler.input
-        if type(input_data) == tuple:
-            if type(input_data[0]) == pd.DataFrame:
-                X, y = input_data
-            else:
-                texts, authors = input_data
+        pipeline = Pipeline([
+            ('combine', FeaturesCombiner()),
+            ('scaler', StandardScaler()),
+            ('model', Model())
+        ])
 
-                # Extract lists of texts, tokens and sentences
-                preprocessor = Preprocessor(texts, authors)
-                texts = preprocessor.texts()
-                tokens = preprocessor.tokens()
-                sentences = preprocessor.sentences()
-                authors = preprocessor.authors
-                X = np.c_[texts, tokens, sentences]
-                y = np.array(authors).ravel()
+        # Run a grid search for searching the best hyper parameters for a selected model
+        param_grid = [
+            {
+                'model__estimator': [RandomForestClassifier()],
+                'model__estimator__n_estimators': [100, 200],
+                'model__estimator__criterion': ['gini', 'entropy'],
+                'model__estimator__max_depth': [2, 6, 10],
+            }
+        ]
+        grid_search = GridSearchCV(pipeline, param_grid, n_jobs=-1, cv=5, verbose=1, scoring='f1_weighted')
+        grid_search.fit(X, y)
 
-                # Extract features from texts, tokens and sentences
-                extractor = FeaturesExtractor(n_jobs=-1)
-                X = extractor.fit_transform(X)
-                # Serialize extracted features if it's necessary
-                if console_handler.features_path:
-                    serialize_features(X, console_handler.features_path, authors=y)
+        print("Best score:", grid_search.best_score_)
+        print('Best params:', grid_search.best_params_)
 
-            # Remove null rows from texts and authors lists
-            notnull_indexes = X.notnull().all(axis=1)
-            X = X[notnull_indexes]
-            y = y[notnull_indexes]
-
-            pipeline = Pipeline([
-                ('combine', FeaturesCombiner()),
-                ('scaler', StandardScaler()),
-                ('model', Model())
-            ])
-
-            # Run a grid search for searching the best hyper parameters for a selected model
-            param_grid = [
-                {
-                    'model__estimator': [RandomForestClassifier()],
-                    'model__estimator__n_estimators': [100, 200],
-                    'model__estimator__criterion': ['gini', 'entropy'],
-                    'model__estimator__max_depth': [2, 6, 10],
-                }
-            ]
-            grid_search = GridSearchCV(pipeline, param_grid, n_jobs=-1, cv=5, verbose=1, scoring='f1_weighted')
-            grid_search.fit(X, y)
-
-            print("Best score:", grid_search.best_score_)
-            print('Best params:', grid_search.best_params_)
-
-            # Serialize the model if it's necessary
-            if console_handler.output:
-                serialize_model(grid_search.best_estimator_._final_estimator, console_handler.output)
+        # Serialize the model if it's necessary
+        if console_handler.output:
+            serialize_model(grid_search.best_estimator_, console_handler.output)
 
     elif console_handler.mode == 'predict':
-        pass
+        model = console_handler.model
+        print('Prediction:', model.predict(X))
 
 
 if __name__ == '__main__':
