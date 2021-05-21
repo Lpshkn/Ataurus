69054cc192b0ca9d82a61a27098540d2c1eb62c1
Lpshkn <lepkirill@yandex.ru>
2021-05-22 00:49:00 +0300

    Update the setup script due to the last changes

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 import ataurus
 from os.path import join, dirname
 from setuptools import setup, find_packages
-from ataurus.configurator.configurator import Configurator
+from ataurus.console_handle.console_handler import ConsoleHandler
 
 setup(
-    name=Configurator.NAME,
+    name=ConsoleHandler.NAME,
     version=ataurus.__version__,
     packages=find_packages(),
-    url='https://github.com/Nikshepel/AuthorshipAttribution',
+    url='https://github.com/Lpshkn/Ataurus/',
     license='',
     author='lpshkn',
     test_suite="tests",
     author_email='lepkirill@yandex.ru',
     long_description=open(join(dirname(__file__), 'README.md')).read(),
-    description=Configurator.DESCRIPTION,
+    description=ConsoleHandler.DESCRIPTION,
     entry_points={
         'console_scripts': ['ataurus = ataurus.main:main']
     }
