9aa79c9d809e4028cc9d0e343054db1bd9cf9bfe
Lpshkn <lepkirill@yandex.ru>
2021-05-21 23:43:24 +0300

    Update the main work of the program for the train mode

--- ataurus/main.py
+++ ataurus/main.py
@@ -7,55 +7,75 @@ import numpy as np
 from preparing.preprocessor import Preprocessor
 from features.extract import FeaturesExtractor
 from features.combine import FeaturesCombiner
-from database.client import Database
 from ml.model import Model
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from serialize.model import serialize_model
+from serialize.features import serialize_features
 
 
 def main():
-    database = Database.connect(['localhost:9200'])
-    authors, texts = database.get_authors_texts('articles_5ath', 'author_nickname', 'text')
-    preprocessor = Preprocessor(texts)
+    console_handler = cfg.ConsoleHandler(sys.argv[1:])
 
-    texts = np.array(preprocessor.texts(), dtype=object)
-    tokens = np.array(preprocessor.tokens(), dtype=object)
-    sentences = np.array(preprocessor.sentences(), dtype=object)
-    X = np.c_[texts, tokens, sentences]
-    y = np.array(authors).ravel()
+    if console_handler.mode == 'train':
+        input_data = console_handler.input
+        if type(input_data) == tuple:
+            if type(input_data[0]) == pd.DataFrame:
+                X, y = input_data
+            else:
+                texts, authors = input_data
 
-    pipeline = Pipeline([
-        ('extracting', FeaturesExtractor()),
-        ('scaler', StandardScaler()),
-        ('model', Model())
-    ])
+                # Extract lists of texts, tokens and sentences
+                preprocessor = Preprocessor(texts, authors)
+                texts = preprocessor.texts()
+                tokens = preprocessor.tokens()
+                sentences = preprocessor.sentences()
+                authors = preprocessor.authors
+                X = np.c_[texts, tokens, sentences]
+                y = np.array(authors).ravel()
 
-    param_grid = [
-        {
-            # Hyper-parameters of the RandomForest
-            'model__estimator': [RandomForestClassifier()],
-            'model__estimator__n_estimators': [50, 100, 150, 200],
-            'model__estimator__criterion': ['gini', 'entropy'],
-            'model__estimator__max_depth': [2, 4, 6, 8, 10],
-            'model__estimator__min_samples_split': [2, 3, 4, 5],
-            'model__estimator__min_samples_leaf': [1, 2, 5, 10]},
-        {
-            # Hyper-parameters of the SVM
-            'model__estimator': [SVC()],
-            'model__estimator__C': [1, 5, 10, 20, 30],
-            'model__estimator__kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
-            'model__estimator__degree': [2, 3, 4]
-        }
-    ]
+                # Extract features from texts, tokens and sentences
+                extractor = FeaturesExtractor(n_jobs=-1)
+                X = extractor.fit_transform(X)
+                # Serialize extracted features if it's necessary
+                if console_handler.features_path:
+                    serialize_features(X, console_handler.features_path, authors=y)
 
-    random_search = RandomizedSearchCV(pipeline, param_grid, n_iter=20, n_jobs=-1, cv=3)
-    random_search.fit(X, y)
-    print(random_search.best_estimator_)
-    print(random_search.best_params_)
-    print(random_search.best_score_)
+            # Remove null rows from texts and authors lists
+            notnull_indexes = X.notnull().all(axis=1)
+            X = X[notnull_indexes]
+            y = y[notnull_indexes]
+
+            pipeline = Pipeline([
+                ('combine', FeaturesCombiner()),
+                ('scaler', StandardScaler()),
+                ('model', Model())
+            ])
+
+            # Run a grid search for searching the best hyper parameters for a selected model
+            param_grid = [
+                {
+                    'model__estimator': [RandomForestClassifier()],
+                    'model__estimator__n_estimators': [100, 200],
+                    'model__estimator__criterion': ['gini', 'entropy'],
+                    'model__estimator__max_depth': [2, 6, 10],
+                }
+            ]
+            grid_search = GridSearchCV(pipeline, param_grid, n_jobs=-1, cv=5, verbose=1, scoring='f1_weighted')
+            grid_search.fit(X, y)
+
+            print("Best score:", grid_search.best_score_)
+            print('Best params:', grid_search.best_params_)
+
+            # Serialize the model if it's necessary
+            if console_handler.output:
+                serialize_model(grid_search.best_estimator_._final_estimator, console_handler.output)
+
+    elif console_handler.mode == 'predict':
+        pass
 
 
 if __name__ == '__main__':
