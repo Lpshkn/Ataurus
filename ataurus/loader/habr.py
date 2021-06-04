0de7dcfa08cea9d5b131577db2d4c3b2bd8c14c4
Lpshkn <lepkirill@yandex.ru>
2021-06-05 01:31:31 +0300

    Add a method to get a list of links by names of authors
    These links are links of articles from Habr.com. They will be parsed further.

--- ataurus/loader/habr.py
+++ ataurus/loader/habr.py
@@ -39,68 +39,40 @@ class Parser:
         # This case allows to determine the max id among all the posts from the site
         self._limit_incorrect = 15
 
-    async def parse(self,
-                    count: int = None,
-                    min_post: int = 1,
-                    max_post: int = 1000000):
+    async def _get_links_by_authors(self, authors: list[str], max_count: int) -> list[str]:
         """
-        Method parses the posts from habr.com and inserts them as documents in the Elasticsearch cluster.
+        Gets a list of links of Habr articles by the author's name.
 
-        :param count: the count of posts that will be inserted in the index
-        :param min_post: the number of the first post to be parsed from
-        :param max_post: the number of the post to which the parsing will be performed
+        :param authors: a list of authors' names
+        :param max_count: the maximum count of articles per an author
+        :return: a list of links
         """
-        # Run a timer
-        logging.warning("Parsing beginning...")
-        begin_time = datetime.now()
+        if max_count is None:
+            max_count = 10**10
 
-        sublists = get_sublists(list(range(min_post, max_post + 1)), self._concurrent)
-        tasks = []
-        for sublist in sublists:
-            tasks.append(self._parse(sublist, count))
-        await asyncio.gather(*tasks)
+        all_links = []
+        for author in authors:
+            author_links = []
+            page_number = 1
+            while True:
+                url = self.URL_USER.format(username=author, page_number=page_number)
+                response = requests.get(url)
 
-        logging.warning('Parsing was completed')
-        logging.warning(f'Work time: {datetime.now() - begin_time}')
-
-
-    async def _parse(self, post_numbers: list[int], count: int):
-        incorrect_count = 0
-
-        for post_number in post_numbers:
-            if count and self._count >= count:
-                break
-
-            url = self.URL.format(post_number=post_number)
-            response = requests.get(url)
-
-            if response.status_code == 200:
-                # Reset the counter
-                # If the counter becomes equal to self._limit_incorrect, parsing ends
-                incorrect_count = 0
-
-                # The count of correct parsed posts
-                self._count += 1
-
-                bs = bs4.BeautifulSoup(response.text, features='html.parser')
-                author = bs.find(class_=self.USERNAME_CLASS).text
-                post_date = bs.find(class_=self.DATETIME_CLASS).get(self.DATETIME_KEY)
-                title = bs.find(class_=self.TITLE_CLASS).text
-                text = bs.find(class_=self.TEXT_CLASS).text
-
-                logging.warning(f"(#{self._count}) id={post_number} {author}: {title}")
-
-                self._database.add(post_number, author, text, title, post_date)
-            elif response.status_code == 404:
-                logging.warning(f"(#{self._count}) id={post_number} Error 404")
-
-                # Increase the counter of incorrect requests
-                incorrect_count += 1
-
-                if incorrect_count == self._limit_incorrect:
+                if response.status_code == 200:
+                    bs = bs4.BeautifulSoup(response.text, features='html.parser')
+                    links = [link.get('href') for link in bs.find_all('a', class_='post__title_link')]
+                    if links:
+                        author_links.extend(links)
+                        if len(author_links) > max_count:
+                            break
+                    else:
+                        break
+                else:
                     break
-            else:
-                logging.warning(f"(#{self._count}) id={post_number} Error {response.status_code}")
 
-            # Take the timeout to avoid a ban
-            await asyncio.sleep(self._timeout + random.randint(0, 5) * random.random())
+                page_number += 1
+
+            all_links.extend(author_links[:max_count])
+
+        return all_links
+
